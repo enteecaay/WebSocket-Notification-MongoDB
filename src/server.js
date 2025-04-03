462f4ae2f961b2df51e77617e0c6eaa87d2e6bd8
enteecaay <kietnguyen060204@gmail.com>
2025-04-03 11:45:53 +0700

    update server.js

--- src/server.js
+++ src/server.js
@@ -70,7 +70,9 @@ app.put("/notifications/read/:userId", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
+app.get("/", (req, res) => {
+  res.send("WebSocket server is running!");
+});
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () =>
   console.log(`🚀 WebSocket Server running on ws://localhost:${PORT}`)
